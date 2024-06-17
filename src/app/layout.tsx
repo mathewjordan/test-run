565caa8050a16d037af8730a5bffc5bcdaaca8e8
mat <mat@northwestern.edu>
2024-06-17 13:36:24 -0400

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import clsx from "clsx";
+
+import { inter } from "@/utils/fonts";
+import { Providers } from "./providers";
+
+import "@radix-ui/themes/styles.css";
+
+import "@/styles/reset.css";
+import "@/styles/globals.css";
+
+export const metadata: Metadata = {
+  title: "Next.js + Radix Themes",
+};
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => (
+  <html
+    lang="en"
+    className={clsx(inter.className, inter.variable)}
+    suppressHydrationWarning
+  >
+    <body>
+      <Providers>{children}</Providers>
+    </body>
+  </html>
+);
+
+export default RootLayout;
